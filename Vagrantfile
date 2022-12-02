a28b333690a5c28a305e92fca92bff435289d00a
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-12-02 10:45:47 +0300

    sharing folders fixed but alpine ignores python installation

--- Vagrantfile
+++ Vagrantfile
@@ -38,7 +38,6 @@ Vagrant.configure(2) do |config|
         SHELL
   
     config.vm.provision "ansible" do |ansible|
-      ansible.verbose = "v"
       ansible.playbook = "playbook.yml"
     end
 
--- pinger.sh
+++ pinger.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if ping -q -c 2 192.168.61.20 2>&1 > /dev/null ; then
-    echo "192.168.61.20 aka lab is OK"
+    echo "192.168.61.20 aka server is OK"
 else
     echo "server ping FAILED"
 fi
 
 if ping -q -c 2 192.168.61.21 2>&1 > /dev/null ; then
-    echo "192.168.61.21 aka web is OK"
+    echo "192.168.61.21 aka client is OK"
 else
     echo "client ping FAILED"
 fi
