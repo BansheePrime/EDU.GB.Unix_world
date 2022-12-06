6092ebab6caf070ee0173eb04e774aeca3779857
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-12-06 14:48:54 +0300

    1, 2, 3, 4, 5, 6, done

--- development/tasks1-10.sh
+++ development/tasks1-10.sh
@@ -1,23 +1,56 @@
 #!/usr/bin/env bash
-
+# Задание 1
 tmux
-
 # ctrl+b % split vertical
 # ctrl+b ':set -g mouse on'
 # 'ls /etc', 'ls /proc', 'ls /home' 
 
+# Задание 2
+
 echo "File 1: $(cat /etc/timezone)" && echo "and ..." && echo "File 2: $(cat /etc/vconsole.conf)" 
 
-man cat | awk "NR==4"
+# Задание 3
 # 'quoting man on cat'
-
+man cat | awk "NR==4"
 cat /etc/timezone > file1.txt && cat /etc/vconsole.conf > file2.txt
-
 cat file2.txt >> file1.txt && cat file1.txt && mv file1.txt result.txt
 
-echo "There is $(ls -A ~ | grep "^\." | wc -l) hidden files in my home dir."
-
+# Задание 4
 for i in {1..3}; do touch geek_file$i.txt; done
 mkdir geek_dir
 mv geek_file*.txt ./geek_dir
 tree -L 2 ./geek_dir/
+echo "There is $(ls -A ~ | grep "^\." | wc -l) hidden files in my home dir."
+
+# Задание 5
+cat /etc/* 2> errors.txt 
+read -p "Press Enter to contunue." && grep 'Permission' errors.txt | wc -l
+
+# Задание 6
+tmux
+vi kill.txt
+ps aux | grep kill.txt | grep -v grep | awk '{print $2}'
+kill $(ps aux | grep kill.txt | grep -v grep | awk '{print $2}')
+
+
+# Задание 7
+?
+
+# Задание 8
+mkdir photos
+cd photos
+for i in {2018..2023}; do mkdir geek_photos_$i; done
+for i in {1..12}; do mkdir ./geek_photos_2018/0$i.month; done
+tree -L 2
+cp -r ./geek_photos_2018/* ./geek_photos_2019 
+cp -r ./geek_photos_2018/* ./geek_photos_2020 
+cp -r ./geek_photos_2018/* ./geek_photos_2021 
+cp -r ./geek_photos_2018/* ./geek_photos_2022 
+cp -r ./geek_photos_2018/* ./geek_photos_2023
+tree -L 2
+
+# Задание 9
+
+
+# Задание 10
+?
