7c4bdf9bde8f915c99dd7745768279b0e7af7efe
BansheePrime <cmapuk-xpehob@yandex.ru>
2022-12-15 13:03:50 +0300

    finish

--- development/task3.sh
+++ development/task3.sh
@@ -6,3 +6,7 @@ mkdir -p 20{10..17}/{01..12}
 for directory in $(find ./ -mindepth 2 -type d); do
     echo $(touch $directory$(tr -dc A-Za-z0-9 </dev/urandom | head -c 12).txt)
 done
+
+for file in $(find ./ -mindepth 2 -type f); do
+    echo $($file >> $file)
+done
